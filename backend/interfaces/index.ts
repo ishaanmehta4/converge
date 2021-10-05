cc510c253b63b79bdf5292e609e1527790439cbf
Ishaan Mehta <ishaanmehta4@gmail.com>
2021-10-05 23:03:53 +0530

    Added TypeDoc

--- backend/interfaces/index.ts
+++ backend/interfaces/index.ts
@@ -1,7 +1,47 @@
 import { Request } from 'express';
+import { Document, Schema } from 'mongoose';
 
 export interface IRequest extends Request {
-    user: {
-        uid: string
-    }
+  user: {
+    uid: string;
+  };
+}
+
+export interface IUser extends Document {
+  username: string;
+  firebase_uid: string;
+  display_name?: string;
+  display_picture?: string;
+  phone_number?: string;
+  email?: string;
+  skills?: [string];
+  fcm_device_tokens?: [string];
+}
+
+enum projectStatus {
+    open,
+    closed
+}
+
+export interface IProject extends Document {
+  author: Schema.Types.ObjectId;
+  title: string;
+  description: string;
+  address?: any;
+  tags?: [string];
+  skills_required?: [string];
+  project_status: projectStatus
+}
+
+enum applicationStatus {
+    pending,
+    accepted,
+    rejected
+}
+
+export interface IApplication extends Document {
+  author: Schema.Types.ObjectId;
+  project: Schema.Types.ObjectId;
+  cover_letter: string;
+  application_status: applicationStatus;
 }
