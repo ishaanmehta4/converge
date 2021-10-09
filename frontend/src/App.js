907f3bf4d2f307eb8194ab77a679445999e9fd8b
Ishaan Mehta <ishaanmehta4@gmail.com>
2021-10-09 17:02:48 +0530

    Added routes for signup and home

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
+import HomePage from './pages/home/home';
+import CompleteSignupPage from './pages/completeSignup/completeSignup';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Switch> 
+          <Route exact path="/" component={HomePage}/>
+          <Route exact path="/complete-signup" component={CompleteSignupPage}/>
+
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
---
+++ frontend/src/pages/completeSignup/completeSignup.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function CompleteSignupPage() {
+    return (
+        <div>
+            <h1>Complete signup</h1>
+        </div>
+    )
+}
+
+export default CompleteSignupPage
