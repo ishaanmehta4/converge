50454e55488fbb69dac0d5b07d75a084f2440d09
Avanish Gupta <avanishcodes@gmail.com>
2021-10-13 16:28:28 +0530

    Add base code for controllers

--- backend/controllers/project.ts
+++ backend/controllers/project.ts
@@ -2,6 +2,13 @@ import { Request, Response } from 'express';
 import { User, Project } from '../models';
 import { IRequest } from '../interfaces';
 
+// Single function to return an error response
+const errorResponse = (res: Response, err: any, code: number = 500) => {
+  res.status(code).json({
+    status: 'error',
+    error: err.message,
+  });
+};
 
 /**
  * Creates and returns a new project document with the data given in req.body.new_project_data
@@ -9,13 +16,21 @@ import { IRequest } from '../interfaces';
  * @param res Express response object
  * @returns response with data of newly created project.
  */
- export async function addProject(req: Request, res: Response) {
-    try {
-    } catch (error) {
-      console.log(error);
-      res.status(500).json({ status: 'error', error });
-    }
-  }  
+export async function addProject(req: Request, res: Response) {
+  try {
+    // Get the project details from the request body
+    const newProjectData = req.body.project;
+    // Create a new project document with the given data
+    const newProject = new Project(newProjectData);
+    // Save the project document
+    await newProject.save();
+    // Return the response with the newly created project data
+    res.status(200).json({ project: newProject });
+  } catch (error) {
+    // Return the error if there is an error
+    errorResponse(res, error);
+  }
+}
 
 /**
  * returns project document with the project_id given in req.params.project_id
@@ -23,14 +38,19 @@ import { IRequest } from '../interfaces';
  * @param res Express response object
  * @returns response with data of required project.
  */
- export async function getProjectData(req: Request, res: Response) {
-    try {
-    } catch (error) {
-      console.log(error);
-      res.status(500).json({ status: 'error', error });
-    }
-  }  
-
+export async function getProjectData(req: Request, res: Response) {
+  try {
+    // Get the project id from the request params
+    const projectId = req.params.project_id;
+    // Find the project document with the given project id
+    const project = await Project.findById(projectId);
+    // Return the response with the project data
+    res.status(200).json({ project });
+  } catch (error) {
+    // Return the error response
+    errorResponse(res, error);
+  }
+}
 
 /**
  * returns all project documents created by current user.
@@ -38,13 +58,25 @@ import { IRequest } from '../interfaces';
  * @param res Express response object
  * @returns response with array of projects documents
  */
- export async function getUserProjects(req: Request, res: Response) {
-    try {
-    } catch (error) {
-      console.log(error);
-      res.status(500).json({ status: 'error', error });
+export async function getUserProjects(req: Request, res: Response) {
+  try {
+    // Get the user id from the request params
+    const userId = req.params.user_id;
+    // Find the user document with the given user id
+    const user = await User.findById(userId);
+    // If the user document is not found, return the error response
+    if (!user) {
+      errorResponse(res, new Error('User not found'));
     }
-  }  
+    // Get all the projects where user is an author
+    const projects = await Project.find({ author: user._id });
+    // Return the response with the projects data
+    res.status(200).json({ projects });
+  } catch (error) {
+    // Return the error response
+    errorResponse(res, error);
+  }
+}
 
 /**
  * Updated the project document with the data given in req.body.updated_data
@@ -52,13 +84,32 @@ import { IRequest } from '../interfaces';
  * @param res Express response object
  * @returns response with updated project document
  */
- export async function updateProject(req: Request, res: Response) {
-    try {
-    } catch (error) {
-      console.log(error);
-      res.status(500).json({ status: 'error', error });
+export async function updateProject(req: Request, res: Response) {
+  try {
+    // Update the project document with the given data
+    const updatedProjectData = req.body.project;
+    // Find the project document with the given project_id
+    const project = await Project.findOne({ _id: updatedProjectData._id });
+    // If the project document is not found, return the error response
+    if (!project) {
+      errorResponse(res, new Error('Project not found'));
     }
-  }  
+    // Update the project document with the given data
+    project.author = updatedProjectData.author;
+    project.title = updatedProjectData.title;
+    project.description = updatedProjectData.description;
+    project.address = updatedProjectData.address;
+    project.tags = updatedProjectData.tags;
+    project.skills_required = updatedProjectData.skills_required;
+    project.project_status = updatedProjectData.project_status;
+
+    // Save the project document
+    await project.save();
+  } catch (error) {
+    // Return the error response
+    errorResponse(res, error);
+  }
+}
 
 /**
  * Deletes a project document, along with all associated applications.
@@ -66,11 +117,18 @@ import { IRequest } from '../interfaces';
  * @param res Express response object
  * @returns response with confirmation of deletion
  */
- export async function deleteProject(req: Request, res: Response) {
-    try {
-    } catch (error) {
-      console.log(error);
-      res.status(500).json({ status: 'error', error });
-    }
-  }  
-
+export async function deleteProject(req: Request, res: Response) {
+  try {
+    // Get the project id from the request params
+    const projectId = req.params.project_id;
+    // Delete the project document
+    await Project.findByIdAndDelete(projectId);
+    // Return the response with confirmation of deletion
+    res.status(200).json({
+      message: 'Project deleted successfully',
+    });
+  } catch (error) {
+    // Return the error response
+    errorResponse(res, error);
+  }
+}
--- backend/controllers/user.ts
+++ backend/controllers/user.ts
@@ -2,6 +2,14 @@ import { Request, Response } from 'express';
 import { User } from '../models';
 import { IRequest } from '../interfaces';
 
+// Single function to return an error response
+const errorResponse = (res: Response, err: any, code: number = 500) => {
+  res.status(code).json({
+    error: err.message,
+  });
+};
+
+// @ts-ignore
 
 /**
  * Creates and returns a new user document with the data given in req.body.new_user_data
@@ -9,23 +17,23 @@ import { IRequest } from '../interfaces';
  * @param res Express response object
  * @returns response with data of newly created user.
  */
- export async function addUser(req: Request, res: Response) {
-    try {
-        const { uid } = req.body;
-        let {new_user_data} = req.body;
+export async function addUser(req: Request, res: Response) {
+  try {
+    const { uid } = req.body;
+    let { new_user_data } = req.body;
 
-        let tempUser = await User.findOne({ firebase_uid: uid });
-        if (tempUser) return res.json({ status: 'error', error: 'User already exists with current firebase UID.' });
-    
-        new_user_data.firebase_uid = uid;
-        let newUser = new User(new_user_data);
-        await newUser.save();
-        res.json({ status: 'success', data: newUser });
-    } catch (error) {
-      console.log(error);
-      res.status(500).json({ status: 'error', error });
-    }
-  }  
+    let tempUser = await User.findOne({ firebase_uid: uid });
+    if (tempUser) return res.json({ status: 'error', error: 'User already exists with current firebase UID.' });
+
+    new_user_data.firebase_uid = uid;
+    let newUser = new User(new_user_data);
+    await newUser.save();
+    res.json({ status: 'success', data: newUser });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ status: 'error', error });
+  }
+}
 
 /**
  * Returns the data of current user based on uid found in JWT.
@@ -75,7 +83,15 @@ export async function getUserData(req: Request, res: Response) {
  */
 export async function updateUser(req: Request, res: Response) {
   try {
-    
+    let { updated_data } = req.body;
+    let currentUser = await User.findOne({ firebase_uid: (req as IRequest).user.uid });
+    if (!currentUser) {
+      return res.status(404).json({ status: 'error', error: 'user/notFound' });
+    }
+    let updatedUser = await User.findOneAndUpdate({ firebase_uid: (req as IRequest).user.uid }, updated_data, {
+      new: true,
+    });
+    return res.json({ status: 'success', data: updatedUser });
   } catch (error) {
     console.log(error);
     res.status(500).json({ status: 'error', error });
@@ -90,7 +106,13 @@ export async function updateUser(req: Request, res: Response) {
  */
 export async function deleteUser(req: Request, res: Response) {
   try {
-  
+    // Get the user document
+    let user = await User.findOne({ firebase_uid: (req as IRequest).user.uid });
+    if (!user) {
+      errorResponse(res, new Error('User not found'), 404);
+    }
+    // Delete the user document
+    await User.findOneAndDelete({ firebase_uid: (req as IRequest).user.uid });
   } catch (error) {
     console.log(error);
     res.status(500).json({ status: 'error', error });
