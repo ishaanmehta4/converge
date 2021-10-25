44891e8376566b7fdd06821adc32b5ea6ce14e4d
Ishaan Mehta <ishaanmehta4@gmail.com>
2021-10-25 22:47:36 +0530

    Added AppBAr UI.

--- frontend/src/components/appBar/AppBar.js
+++ frontend/src/components/appBar/AppBar.js
@@ -1,15 +1,20 @@
 import React from 'react'
 import Button from '@mui/material/Button';
-import DeleteIcon from '@mui/icons-material/Delete';
+import LogoutIcon from '@mui/icons-material/Logout';
 
 import './style.scss'
+
+function logOutUser() {
+    localStorage.removeItem('token')
+    window.location.href = '/'
+}
 
 function AppBar() {
     return (
         <div className="app-nav-wrapper">
             <div>CONVERGE</div>
             <div>
-                <Button size="small" variant="text" startIcon={<DeleteIcon />}> logout</Button>
+                <Button onClick={logOutUser} size="small" color="error" variant="text" startIcon={<LogoutIcon />}> logout</Button>
             </div>
         </div>
     )
